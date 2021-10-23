47da701b90265fc75ee0c8893f14df899539584b
DCay <atriumandvintrigol@gmail.com>
2021-10-23 09:16:39 +0300

    Removed Exam Prep

---
+++ Advanced/Advanced.BiConnectivity/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advanced.BiConnectivity
+{
+    class Program
+    {
+        static void Dfs(int currentNode, Dictionary<int, List<int>> graph, Dictionary<int, bool> isVisited, Action<int> action)
+        {
+            if (!isVisited[currentNode])
+            {
+                isVisited[currentNode] = true;
+
+                // Process current node
+                action.Invoke(currentNode);
+
+                foreach (var childNode in graph[currentNode])
+                {
+                    Dfs(childNode, graph, isVisited, action);
+                }
+            }
+        }
+
+        static int GetConnectedComponentsCount(Dictionary<int, List<int>> graph)
+        {
+            int connectedComponents = 0;
+
+            Dictionary<int, bool> isVisited = new Dictionary<int, bool>();
+
+            foreach (var item in graph)
+            {
+                isVisited.Add(item.Key, false);
+            }
+
+            foreach (var node in graph)
+            {
+                if (!isVisited[node.Key])
+                {
+                    connectedComponents++;
+                    Dfs(node.Key, graph, isVisited, (node) => { });
+                }
+            }
+
+            return connectedComponents;
+        }
+
+        static Dictionary<int, List<int>> CopyGraph(Dictionary<int, List<int>> graph)
+        {
+            Dictionary<int, List<int>> copyGraph = new Dictionary<int, List<int>>();
+
+            foreach (var node in graph)
+            {
+                copyGraph.Add(node.Key, new List<int>(node.Value));
+            }
+
+            return copyGraph;
+        }
+
+        static int GetBiConnectedComponentsCount(Dictionary<int, List<int>> graph)
+        {
+            int articulationPointsCount = 0;
+
+            foreach (var node in graph)
+            {
+                Dictionary<int, List<int>> temporaryGraph = CopyGraph(graph);
+
+                // Temprorary Removal of Node
+                temporaryGraph.Remove(node.Key);
+
+                foreach (var temporaryNode in temporaryGraph)
+                {
+                    temporaryNode.Value.Remove(node.Key);
+                }
+
+                int connectedComponentsCount = GetConnectedComponentsCount(temporaryGraph);
+
+                if (connectedComponentsCount > 1)
+                {
+                    articulationPointsCount++;
+                }
+            }
+
+            return articulationPointsCount; // Articulation Points
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+            Dictionary<int, bool> isVisited = new Dictionary<int, bool>();
+
+            int[] nodes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            //Initialization
+            foreach (var node in nodes)
+            {
+                graph.Add(node, new List<int>());
+            }
+
+            string inputLine = string.Empty;
+
+            while ((inputLine = Console.ReadLine()) != "End")
+            {
+                string[] inputParams = inputLine.Split();
+
+                int parentNode = int.Parse(inputParams[0]);
+                int childNode = int.Parse(inputParams[1]);
+
+                graph[parentNode].Add(childNode);
+                graph[childNode].Add(parentNode);
+            }
+
+            // Slow Algorithm
+            Console.WriteLine(GetBiConnectedComponentsCount(graph) + 2);
+        }
+    }
+}
---
+++ Advanced/Advanced.Testing/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace Advanced.Testing
+{
+    class Program
+    {
+        /*
+        //
+        ================================ Case - 15
+
+        //1 2 3 4
+        //5 6
+
+        1 2 3 4 5 6
+        1 2 3 5 4 6
+        1 2 3 5 6 4
+
+        1 2 5 3 4 6
+        1 2 5 6 3 4
+
+        1 5 2 3 4 6
+        1 5 6 2 3 4
+
+        1 2 5 3 6 4
+
+        1 5 2 6 3 4
+        1 5 2 3 6 4
+
+        5 6 1 2 3 4
+        5 1 6 2 3 4
+        5 1 2 6 3 4
+        5 1 2 3 6 4
+        5 1 2 3 4 6
+
+        ================================ CASE - 10
+
+        // 1 2 3
+        // 4 5
+        
+        // 1 2 3 4 5
+        // 1 2 4 3 5
+        // 1 2 4 5 3
+
+        // 1 4 2 3 5
+        // 1 4 5 2 3 
+        // 1 4 2 5 3
+        
+        // 4 5 1 2 3
+        // 4 1 5 2 3
+        // 4 1 2 5 3
+        // 4 1 2 3 5
+        
+         
+
+        //================================== Case - 6
+
+        //1 2 3 4 | 0 1 100 101
+        //1 3 2 4 | 0 100 1 101
+        //1 3 4 2 | 0 100 101 1
+        //3 4 1 2 | 100 101 0 1
+        //3 1 4 2 | 100 0 101 1
+        //3 1 2 4 | 100 0 1 101
+        */
+        // ============================================ CASE - 20
+        /* 
+         * 
+         * 1 2 3 4 5 6
+         * 1 2 4 3 5 6
+         * 1 2 4 5 3 6
+         * 1 2 4 5 6 3
+         * 
+         * 1 4 2 3 5 6
+         * 1 4 5 2 3 6
+         * 1 4 5 6 2 3
+         * 1 4 5 2 6 3
+         * 
+         * 1 4 2 5 3 6
+         * 1 4 2 5 6 3
+         * 
+         * 4 1 2 3 5 6
+         * 4 5 1 2 3 6
+         * 4 5 6 1 2 3
+         * 
+         * 4 1 2 5 3 6
+         * 4 1 2 5 6 3
+         * 
+         * 4 1 5 2 3 6
+         * 4 1 5 2 6 3
+         * 4 1 5 6 2 3
+         * 
+         * 4 5 1 6 2 3
+         * 4 5 1 2 6 3
+         * 
+         */
+
+
+        static List<string> output = new List<string>();
+
+        static void Generate(int index,string[] str, bool[] used, List<string> currentCombo)
+        {
+            if(index >= str.Length)
+            {
+                output.Add(string.Join(" ", currentCombo));
+            }
+            else
+            {
+                for (int i = index; i < str.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        currentCombo.Add(str[i]);
+                        Generate(index + 1, str, used, currentCombo);
+                        currentCombo.RemoveAt(currentCombo.Count - 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        static List<string> solve(string s)
+        {
+            output = new List<string>();
+
+            string[] arr = s.Split().ToArray();
+
+            Generate(0, arr, new bool[arr.Length], new List<string>());
+
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int m = 100;
+
+            BigInteger bigIntegerN = BigInteger.One;
+            BigInteger bigIntegerM = BigInteger.One;
+
+
+            for (int i = n; i >= 1; i--)
+            {
+                bigIntegerN *= i;
+            }
+
+            for (int i = m; i >= 1; i--)
+            {
+                bigIntegerM *= i;
+            }
+
+
+        }
+    }
+}
