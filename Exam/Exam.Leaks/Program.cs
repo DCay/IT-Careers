2ee69b785f31322e578a222f3222797558f997bf
DCay <atriumandvintrigol@gmail.com>
2021-11-05 19:59:09 +0200

    Added exam prep

---
+++ Exam/Exam.Leaks/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.Leaks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>();
+
+            string inputLine = string.Empty;
+
+            while ((inputLine = Console.ReadLine()) != "end")
+            {
+                string[] inputParams = inputLine.Split();
+
+                if (!graph.ContainsKey(inputParams[0]))
+                {
+                    graph[inputParams[0]] = new List<string>();
+                }
+
+                if (!graph.ContainsKey(inputParams[1]))
+                {
+                    graph[inputParams[1]] = new List<string>();
+                }
+
+                graph[inputParams[0]].Add(inputParams[1]);
+            }
+
+            HashSet<string> traversedConnections = new HashSet<string>();
+
+            int count = 0;
+
+            graph
+                .Where(node => graph.Any(otherNode => otherNode.Value.Contains(node.Key)))
+                .Where(node => node.Value.Any(childNode => graph[childNode].Contains(node.Key)))
+                .ToList().ForEach(node =>
+                {
+                    foreach (var child in node.Value)
+                    {
+                        string connection = $"{node.Key} <-> {child}";
+                        if (!traversedConnections.Contains(connection))
+                        {
+                            traversedConnections.Add(connection);
+                            traversedConnections.Add($"{child} <-> {node.Key}");
+                            count++;
+                            Console.WriteLine(connection);
+                        }
+                    }
+                });
+
+            if(count == 0)
+            {
+                Console.WriteLine("Disconnected");
+            }
+        }
+    }
+}
---
+++ Exam/Exam.Permutations/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Exam.Permutations
+{
+    class Program
+    {
+        static void Permute(int index, string[] set, bool[] used, LinkedList<string> currentCombo, HashSet<string> combos)
+        {
+            if(index == set.Length)
+            {
+                combos.Add(string.Join("", currentCombo));
+            }
+            else
+            {
+                for (int i = 0; i < set.Length; i++)
+                {
+                    if(!used[i])
+                    {
+                        used[i] = true;
+                        currentCombo.AddLast(set[i]);
+                        Permute(index + 1, set, used, currentCombo, combos);
+                        currentCombo.RemoveLast();
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] set = Console.ReadLine().Split();
+            
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            HashSet<string> setOfChars = new HashSet<string>(set);
+
+            HashSet<string> combos = new HashSet<string>();
+
+            Permute(0, set, new bool[set.Length], new LinkedList<string>(), combos);
+
+            bool hasAny = false;
+
+            string inputLine = string.Empty;
+
+            LinkedList<string> finalResult = new LinkedList<string>();
+
+            while ((inputLine = Console.ReadLine()) != "end")
+            {
+                if(combos.Count == 0)
+                {
+                    break;
+                }
+
+                if(combos.Contains(inputLine))
+                {
+                    hasAny = true;
+                    finalResult.AddLast(inputLine);
+                    combos.Remove(inputLine);
+                }
+            }
+
+            if (hasAny)
+            {
+                finalResult.OrderBy(x => x).ToList().ForEach(x => Console.WriteLine(string.Join(" ", x.ToCharArray())));
+            }
+            else
+            {
+                Console.WriteLine("No permutations...");
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds}ms");
+        }
+    }
+}
